e4ee7925cfdc15c286604e091dcd4ce1b40e152a
Rene-Zaltbommel <91453184+Rene-Zaltbommel@users.noreply.github.com>
2021-12-16 10:16:04 +0100

    Add files via upload

---
+++ zesdeJava/index.js
@@ -0,0 +1,22 @@
+const makePizza = function (){
+    console.log('maak vuur in de pizzaoven');
+    console.log('maak het deeg plat');
+    console.log('doe er kaas op');
+    console.log('doe er saus op');
+    console.log('doe er tonijn op');
+    console.log('plaats hem in de oven');
+
+};
+
+const makeSushi = function(){
+    console.log('snij de zalm');
+    console.log('kook de rijst');
+    console.log('wikkel er zweewier omheen');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
